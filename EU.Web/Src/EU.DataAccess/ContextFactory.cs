7f0a2f2ce3404cd446d9d4af5e54cab6fd43be9d
xiaochanghai <xiaochanghai@foxmail.com>
2023-06-02 17:39:40 +0800

    新增创建 DBContext

---
+++ EU.Web/Src/EU.DataAccess/ContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EU.Core.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace EU.DataAccess
+{
+    /// <summary>
+    /// 直接创建 HDISContext
+    /// </summary>
+    public class ContextFactory
+    {
+        /// <summary>
+        /// 创建DbContext
+        /// </summary>
+        /// <returns></returns>
+        public static DataContext CreateContext()
+        {
+            var builder = new DbContextOptionsBuilder<DataContext>();
+
+            builder.UseSqlServer(AppSetting.DbConnectionString);
+            return new DataContext(builder.Options);
+        }
+    }
+}
